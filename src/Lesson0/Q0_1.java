11c1cd74ac3ba0e9da8170ec26a89a8aa8bdeac0
ShunHidaka <hidakashun60@gmail.com>
2024-01-18 16:56:54 +0900

    自己学習

---
+++ src/Lesson0/Q0_1.java
@@ -0,0 +1,11 @@
+package Lesson0;
+
+public class Q0_1 {
+
+	public static void main(String[] args) {
+		// HelloWorldを出力
+
+		System.out.println("HelloWorld");
+	}
+
+}
---
+++ src/Lesson0/Q0_3.java
@@ -0,0 +1,33 @@
+package Lesson0;
+
+public class Q0_3 {
+
+	public static void main(String[] args) {
+		// 計算させる
+		
+		System.out.print("39 + 2 = ");
+		System.out.print(39 + 2);//足し算
+		
+		System.out.println();//改行
+		
+		System.out.print("39 - 2 = ");
+		System.out.print(39 - 2);//引き算
+		
+		System.out.println();//改行
+		
+		System.out.print("-5 * 7 = ");
+		System.out.print(-5 * 7);//掛け算
+		
+		System.out.println();//改行
+		
+		System.out.print("6 * 6 * 3.14 = ");
+		System.out.print(6 * 6 * 3.14);//掛け算②
+		
+		System.out.println();//改行
+		
+		System.out.print("答えは" + 64);//文字列型と整数型の連結
+		
+
+	}
+
+}
